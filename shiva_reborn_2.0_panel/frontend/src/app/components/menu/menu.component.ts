6e5d31cf2c3458f79d8dc119047c243027e5a354
Vlad Valean <vlad.valean19@gmail.com>
2022-05-27 21:32:11 +0300

    WIP menu-form

--- shiva_reborn_2.0_panel/frontend/src/app/components/menu/menu.component.ts
+++ shiva_reborn_2.0_panel/frontend/src/app/components/menu/menu.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {FormControl} from '@angular/forms';
+
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
-
+export class MenuComponent {
   constructor() { }
-
-  ngOnInit(): void {
-  }
+  countryControl = new FormControl();
+  countries: string[] = ['Romania', 'Danemarca', 'Spania'];
+  cityControl = new FormControl();
+  cities: string[] = ['Oras1', 'Oras2', 'Oras3'];
+  floorControl = new FormControl();
+  floors: string[] = ['Etaj1', 'Etaj2', 'Etaj3'];
 
 }
