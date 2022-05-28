4485c938ce4373330f718d55b62fde0f9f757327
Vlad Valean <vlad.valean19@gmail.com>
2022-05-28 18:25:14 +0300

    login should work

--- ShivaReborn.DataAccess/Models/User.cs
+++ ShivaReborn.DataAccess/Models/User.cs
@@ -1,13 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
 
 namespace ShivaReborn.DataAccess.Models;
 
-public class User : BaseEntity
+public class User : IdentityUser
 {
-    public string firstName { get; set; }
-    public string lastName { get; set; }
-    public string email { get; set; }
-    [ForeignKey("Place")] string assignedPlaceId { get; set; }
-    private bool isAdmin { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    [ForeignKey("Place")] string AssignedPlaceId { get; set; }
+    private bool IsAdmin { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Updated { get; set; }
 }
